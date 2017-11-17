0b22565dde282e10977141f321369bfe4b71ce1e
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-11-17 13:28:12 -0800

    Set up one-to-many relationship between reviews and products.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
   validates :name, :cost, :origin, :presence => true
+
+  has_many :reviews
 end
---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ActiveRecord::Base
+  validates :author, :content_body, :rating, :product_id, :presence => true
+
+  belongs_to :product
+end
